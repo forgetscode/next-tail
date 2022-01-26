2575a94ba32d9480f73f659cda1408ec6a0c00d7
michael <forgetscode@gmail.com>
2022-01-26 01:56:16 -0800

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import SideBar from '../components/SideBar';
+import { Box } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+      <>
+        <SideBar></SideBar>
+        <div className='ml-20'>
+          <Component {...pageProps}   />
+        </div>
+      </>
+    );
 }
 
 export default MyApp
